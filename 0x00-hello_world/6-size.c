814ee7a5150bab90f703ea415a06ba5de0d3053c
ddanimoh <ddanimoh@gmail.com>
2023-02-16 09:36:13 -0800

    Fixed to recommit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-/* comment for comments only*/
+/* commenting comment on comment testing test*/
 
 int main(void)
 {
-	printf("Size of a char: %c byte(s)", sizeof(char));
-	printf("Size of an int: %d byte(s)", sizeof(int));
-	printf("Size of a long int: %d byte(s)", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
-	printf("Size of a float: %f byte(s)", sizeof(float));
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
