5d386d8111e4ed5dfc0c6a06bb8011a9907cb668
Danimohlife <122839040+ddanimoh@users.noreply.github.com>
2023-02-22 23:38:56 +0100

    Delete 0x02-functions_nested_loops directory

--- 0x02-functions_nested_loops/README.md
+++
@@ -1,1 +0,0 @@
-Third project on C
--- 0x02-functions_nested_loops/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int print_sign(int n);
-int _isalpha(int c);
-#endif
