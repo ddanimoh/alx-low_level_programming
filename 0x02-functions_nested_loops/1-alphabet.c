fae9bb61d7bf06a0be388949e02ed7537d2766d0
ddanimoh <ddanimoh@gmail.com>
2023-02-21 17:50:25 -0800

    10ne

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
+#include "main.h"
 
-int main
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: no return Value
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+}
