cc527e63cbe248fd6cd52455c6b79e04af10f996
ddanimoh <ddanimoh@gmail.com>
2023-02-22 22:32:15 -0800

    added commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,5 +8,9 @@ int _abs(int);
 int print_sign(int n);
 int _isalpha(int c);
 int print_last_digit(int);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
 
 #endif
