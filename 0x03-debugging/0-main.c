8facb2c5c423c36b1ee3601f4dfe00fef2792d0a
ddanimoh <ddanimoh@gmail.com>
2023-02-23 01:49:15 -0800

    All

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - for calling and other function
- * Return: O (success)
- */
-int main()
+  * main - Test function for positive or negative
+  * Return: 0
+  */
+
+int main(void)
 {
-	positive_or_negative(0);
-	return (0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
 }
