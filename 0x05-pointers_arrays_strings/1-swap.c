fc78b3097c273b6cdbe187e9c939b2e4caa144b8
ddanimoh <ddanimoh@gmail.com>
2023-02-27 18:32:20 -0800

    swp

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ * @a: is a pointer
+ * @b: is also a pointer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
