df1c55189b196b449ea089ac51432d767b9db8e9
ddanimoh <ddanimoh@gmail.com>
2023-03-07 22:02:01 -0800

    genius

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts_recursion - print a string
  * @s:string
@@ -8,11 +9,11 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		putchar(*s);
+		puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 }
