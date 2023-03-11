c68cc33cd66cd78caefc6a992d40037dafe83619
ddanimoh <ddanimoh@gmail.com>
2023-03-10 18:12:04 -0800

    argument or parametr

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program that adds positive numbers
+ * @args: This is the argument
+ * @argv: This is the argument
+ *
+ * Return: 0;
+ */
+int main(int args, char *argv[])
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+	return (0);
+}
