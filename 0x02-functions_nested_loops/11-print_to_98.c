8746e5fbc7636300aca9922374fd436e797cf36d
ddanimoh <ddanimoh@gmail.com>
2023-04-22 11:10:11 -0700

    11-print_to_98 adjust

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,19 +13,7 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			if (n > 99)
-			{
-				i = n / 10;
-
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar((n % 10) + '0');
-			}
-			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
+			printf("d%", n);
 			if(n != 98)
 			{
 				_putchar(',');
@@ -37,42 +25,12 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			if (n >= 0)
+			printf("d%", n);
+			if(n != 98)
 			{
-				if (n >= 0 && n < 10)
-				{
-					_putchar((n % 10) + '0');
-				}
-				else
-				{
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-				}
-				if (n != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(',');
+				_putchar(' ');
 			}
-			else
-			{
-				if (n < 0 && n < -9)
-				{
-					_putchar((n / 10) + 0);
-					_putchar((n % 10) + 0);
-
-				}
-				else
-				{
-					printf("%d", n);
-				}
-				if (n != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-
 		}
 	}
 }
