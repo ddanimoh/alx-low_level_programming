1e61adfedc34847a7f630f229c2b95487ee12882
ddanimoh <ddanimoh@gmail.com>
2023-04-22 11:01:34 -0700

    11-print_to_98 adjust

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,15 @@
  */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n >= 98)
 	{
 		for (; n >= 98; n--)
 		{
-			int i;
-
 			if (n > 99)
 			{
-				i = n % 10;
+				i = n / 10;
 
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
@@ -33,7 +33,7 @@ void print_to_98(int n)
 			}
 		}
 	}
-	else
+	else if (n < 98)
 	{
 		for (; n <= 98; n++)
 		{
@@ -48,22 +48,23 @@ void print_to_98(int n)
 					_putchar((n / 10) + '0');
 					_putchar((n % 10) + '0');
 				}
+				if (n != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			else
 			{
-				if (n < 0)
-					_putchar('-');
 				if (n < 0 && n < -9)
 				{
-					n = -n;
 					_putchar((n / 10) + 0);
 					_putchar((n % 10) + 0);
 
 				}
 				else
 				{
-					n = -n;
-					_putchar((n % 10) + 0);
+					printf("%d", n);
 				}
 				if (n != 98)
 				{
