0859b5b4b6001b80cb90833890a0c8b5001995d6
ddanimoh <ddanimoh@gmail.com>
2023-05-03 20:33:27 -0700

    rmv

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - remove node from top
+ * Return: top node
+ * @head: point to node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int rmvtop;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	ptr = (*head)->next;
+	rmvtop = (*head)->next;
+	free(*head);
+	*head = ptr;
+
+	return (rmvtop);
+}
