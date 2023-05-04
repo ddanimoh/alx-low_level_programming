94e63c67fb023e9dd6b969fe25a149444a842dbd
ddanimoh <ddanimoh@gmail.com>
2023-05-03 17:35:58 -0700

    num of element

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - num of elemnet
+ * @h: str
+ * Return: num of element
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num_of_elements = 0;
+
+	while (h != NULL)
+	{
+		num_of_elements++;
+		h = h->next;
+	}
+	return (num_of_elements);
+}
