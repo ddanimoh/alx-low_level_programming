b925ffbb332db12b314b608c9b4127f8bbf16bb8
ddanimoh <ddanimoh@gmail.com>
2023-05-03 20:11:27 -0700

    free **H

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,8 +13,8 @@ void free_listint2(listint_t **head)
 	}
 	while (*head != NULL)
 	{
-		used = *(head)->next;
-		free(*(head));
+		used = (*head)->next;
+		free(*head);
 		*head = used;
 	}
 	*head = NULL;
