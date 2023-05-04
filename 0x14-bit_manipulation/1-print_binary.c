80e93aec1d91ea86b096a6cc12d7a8449abab2a0
ddanimoh <ddanimoh@gmail.com>
2023-05-04 16:12:40 -0700

    binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - funct to print binary
+ * @a: Num to convert
+ * Return: binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int p;
+	int x, y = 0;
+
+	for (x = 63; x >= 0; x--)
+	{
+		p = n >> x;
+
+		if (p & 1)
+		{
+			_putchar('1');
+			y = y + 1;
+		}
+		else if (y)
+		{
+			_putchar('0');
+		}
+	}
+	if (!y)
+		_putchar('0');
+}
