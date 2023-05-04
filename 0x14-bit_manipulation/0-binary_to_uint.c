4dac5c44c35bff9ca52f5c2868d862ac17eaca3f
ddanimoh <ddanimoh@gmail.com>
2023-05-04 15:12:41 -0700

    binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * binary_to_uint - funt to decimal
+ * @b: point to char values
+ * Return: decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int y;
+	int x;
+
+	y = 0;
+
+	if (*b == '\0')
+	{
+		return (0);
+	}
+
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] != '1' && b[x] != '0')
+		{
+			return (0);
+		}
+
+		y <<= 1;
+
+		if (b[x] == '1')
+		{
+			y = y + 1;
+		}
+	}
+	return (y);
+}
