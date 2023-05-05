3e0eecc08abd94fa6149e14beb03b0afbd2fe2ab
ddanimoh <ddanimoh@gmail.com>
2023-05-05 05:26:49 -0700

    binary

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,6 +9,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	*n = (*n | (1UL << index));	
+	*n |= (1UL << index);
 	return (1);
 }
