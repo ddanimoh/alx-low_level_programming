cd96b9943e16ba7f66803feda1d31bab2394b96c
ddanimoh <ddanimoh@gmail.com>
2023-05-13 00:27:28 +0100

    file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * append_text_to_file - funct 
+ * append_text_to_file - funct
  * @filename: name of the file
  * @text_content: the NULL
  * Return: 1 / -1
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	
+
 	if (text_content == NULL)
 	{
 		text_content = "";
