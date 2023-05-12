3976e6f8a44245a34c1858d2a44d3847be483734
ddanimoh <ddanimoh@gmail.com>
2023-05-12 21:52:04 +0100

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,11 +11,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *memo;
 	ssize_t read_fn, write_fn, open_fn;
 
-	if (filename == NULL)
-	{
-		return (0);
-	}
-
 	memo = malloc(letters * sizeof(char));
 
 	if (memo == NULL)
@@ -23,16 +18,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	open_fn = open(filename, O_RDONLY);
-	read_fn = read(open_fn, memo, letters);
-	write_fn = write(STDOUT_FILENO, memo, read_fn);
 
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	open_fn = open(filename, O_RDONLY);
 
 	if (read_fn == -1 || write_fn != read_fn || write_fn == -1 || open_fn == -1)
 	{
 		free(memo);
 		return (0);
 	}
-	free(memo);
+
+	read_fn = read(open_fn, memo, letters);
+
+	write_fn = write(STDOUT_FILENO, memo, read_fn);
+
+	close(open_fn);
 	return (write_fn);
 }
