59abb13a69bd6d848686cf72a18860ae6caf3422
ddanimoh <ddanimoh@gmail.com>
2024-05-11 17:29:57 +0100

    udated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = ptr;
 		return (ptr);
 	}
-	if (head == NULL && idx == 0)
+	if (*head == NULL && idx == 0)
 	{
 		*head = ptr;
 		return (ptr);
